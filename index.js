a88f2cb741fcec82d36379c7a13b7566c7a51e6c
Vegar Norman <code@vegar.codes>
2026-02-03 19:36:39 +0100

    Legg på endepunkter for statuskode og delay

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config({ quiet: true });
 
 console.log(`Crudops versjon ${packageJson.version}\n\n`);
 
+/* Sjekker om miljøvariablene i .env er spesifisert */
 if (!process.env.TEMPLATE) {
   console.error("Fatal feil: du må spesifisere en template som skal brukes som en miljøvariabel kalt TEMPLATE. Les dokumentasjonen for mer info.");
   process.exit(1);
@@ -17,11 +18,13 @@ if (!process.env.API_KEY) {
   process.exit(1);
 }
 
+/* Sjekker om TEMPLATE er korrekt */
 if (!fs.existsSync(`./templates/${process.env.TEMPLATE}`)) {
   console.error(`Fatal feil: Den angitte templaten (${process.env.TEMPLATE}) ligger ikke i templates-mappa. Pass på at du bare skriver filnavnet som skal brukes, for eksempel "testdata.json".`);
   process.exit(1);
 }
 
+/* Dersom databasefilen ikke eksisterer, kopier templaten til db.json */
 if (!fs.existsSync("./db.json")) {
   console.log(`Databasefilen finnes ikke — kopierer fra angitt template.\n\n`);
   fs.copyFileSync(`./templates/${process.env.TEMPLATE}`, "./db.json");
@@ -72,6 +75,52 @@ server.post("/api/reset", (request, response) => {
   router.db.setState(resetData);
   response.status(200).send();
 });
+
+/* Endepunkt: Bytt ut :code med ønsket HTTP-statuskode, og APIet sender en respons tilbake med den statuskoden. Nyttig for testing av 404, 500 og så videre. */
+server.all("/api/status/:code", (request, response) => {
+  const code = Number(request.params.code);
+  
+  if (code !== NaN && typeof code === "number" && code >= 200 && code <= 599) {
+    response
+    .status(Number(code))
+    .json({
+      statusCode: code,
+      message: `Hei! Du har bedt om statuskode ${code}`,
+      headers: request.headers,
+      body: request.body
+    });
+  } else {
+    response
+    .status(400)
+    .json({
+      message: "Du har angitt en ugyldig statuskode. Pass på at statuskoden er mellom 200 og 599."
+    });
+  }
+});
+
+/* Endepunkt: Send respons etter spesifisert antall millisekunder. */
+server.all("/api/delay/:ms", (request, response) => {
+  const ms = Number(request.params.ms);
+
+  if (ms !== NaN && typeof ms === "number" && ms > 0) {
+    setTimeout(() => {
+      response
+        .status(200)
+        .json({
+          message: `Respons sendt etter ${Math.ceil(ms)} millisekunder.`,
+          headers: request.headers,
+          body: request.body
+        });
+    }, Math.ceil(ms));
+  } else {
+    response
+    .status(400)
+    .json({
+      message: "Du har angitt et ugyldig antall millisekunder. Pass på at du angir et heltall over 0."
+    });
+  }
+});
+
 
 server.use("/api", router);
 
